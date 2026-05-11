3dba827d12949668959fcc43b11186eeafaff9f0
Bhavya Sree <bhavyasreekuppam@gmail.com>
2026-05-11 18:51:01 +0530

    Use relative component imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Navbar from '@/components/Navbar'
-import HeroBanner from '@/components/HeroBanner'
-import TrendingPlants from '@/components/TrendingPlants'
-import TopSelling from '@/components/TopSelling'
-import CustomerReviews from '@/components/CustomerReviews'
-import O2Plants from '@/components/O2Plants'
-import Footer from '@/components/Footer'
+import Navbar from '../components/Navbar'
+import HeroBanner from '../components/HeroBanner'
+import TrendingPlants from '../components/TrendingPlants'
+import TopSelling from '../components/TopSelling'
+import CustomerReviews from '../components/CustomerReviews'
+import O2Plants from '../components/O2Plants'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
